076911f9bc0f234ae60f6dfd35b28f22a60a80f6
Mehdi83 <Mehdy1982_me@yahoo.com>
2022-11-29 12:39:52 -0600

    Docker 1-st project

---
+++ html/index.php
@@ -0,0 +1,9 @@
+    <!DOCTYPE html>  
+     <head>  
+      <title>PHP</title>
+     </head>  
+
+     <body>  
+      <h1>Hello World!!!!</h1>
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+     </body>
